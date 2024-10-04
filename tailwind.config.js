406041c301b7c1fc3ebbd6498c787a76ac2f0948
JadenFicklin <jadendficklin@gmail.com>
2024-10-04 17:05:06 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    screens: {
+      xs: '290px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1200px',
+      xxl: '1350px',
+      xxxl: '1400px'
+    },
+    extend: {}
+  },
+  plugins: []
+};
